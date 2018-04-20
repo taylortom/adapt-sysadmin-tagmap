004adbbee6873bd624519ab2f753b19add4b6293
Thomas Taylor <thomas.taylor@kineo.com>
2018-04-20 16:39:46 +0100

    Add initial files

---
+++ README.md
@@ -0,0 +1,14 @@
+# adapt-sysadmin-tagmap
+
+This is an Adapt **authoring tool plugin** which displays a list of tags with their associated courses, and is **for Super Admin only**.
+
+Note that this is a child plugin of the [adapt-sysadmin](https://github.com/taylortom/adapt-sysadmin/) plugin, and as such, will not work without it.
+
+## Installation
+
+1. Copy `sysadmin-tagmap` to `/frontend/src/plugins/` in your authoring tool folder.
+2. That’s it!
+
+## Dependencies
+
+- [adapt-sysadmin](https://github.com/taylortom/adapt-sysadmin)
---
+++ sysadmin-tagmap/views/tagMapView.js
@@ -0,0 +1,60 @@
+// LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
+define(function(require) {
+  var Backbone = require('backbone');
+  var Origin = require('core/origin');
+  var SysadminView = require('plugins/sysadmin/views/sysadminPluginView');
+  var ContentCollection = require('core/collections/contentCollection');
+  var TagCollection = Backbone.Collection.extend({ url: '/api/content/tag' });
+
+  var TAG_PREFIX = 'test-'
+
+  var TagMapView = SysadminView.extend({
+    name: 'tagmap',
+    settings: {
+      autoRender: false
+    },
+
+    initialize: function(options) {
+      this.model = new Backbone.Model({
+        tags: new TagCollection(),
+        courses: new ContentCollection(null, { _type: 'course' })
+      });
+      SysadminView.prototype.initialize.apply(this, arguments);
+    },
+
+    preRender: function() {
+      this.fetchTags(function() {
+        this.model.get('tags').each(function(tag) {
+          var tagId = tag.get('_id');
+          tag.set('courses', this.model.get('courses').filter(function(course) {
+            return _.some(course.get('tags'), function(tag) { return tag._id === tagId });
+          }));
+        }, this);
+        console.log(this.model.get('tags'));
+        this.render();
+      });
+    },
+
+    fetchTags: function(cb) {
+      this.model.get('tags').fetch({
+        data: { title: { $regex: '^' + TAG_PREFIX } },
+        success: _.bind(function() {
+          this.fetchCourses(cb);
+        }, this)
+      });
+    },
+
+    fetchCourses: function(cb) {
+      this.model.get('courses').fetch({
+        data: {
+          $or: this.model.get('tags').map(function(tag) {
+            return { tags: tag.get('_id') };
+          })
+        },
+        success: _.bind(function() { cb.call(this); }, this)
+      });
+    }
+  }, { template: 'tagMap' });
+
+  return TagMapView;
+});
