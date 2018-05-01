b98ed6749c2212ab70adf20318c5b245b1eda7ad
Tom Taylor <hello@tomtaylor.name>
2018-05-01 20:58:48 +0100

    Add support for dual tags

--- globalMenu-tagmap/views/tagMapView.js
+++ globalMenu-tagmap/views/tagMapView.js
@@ -6,7 +6,7 @@ define(function(require) {
   var ContentCollection = require('core/collections/contentCollection');
   var TagCollection = Backbone.Collection.extend({ url: '/api/content/tag' });
 
-  var TAG_PREFIX = 'test-'
+  var TAG_PREFIX = '(p|m)-';
 
   var TagMapView = OriginView.extend({
     className: 'tagmap',
@@ -24,24 +24,28 @@ define(function(require) {
 
     preRender: function() {
       this.fetchTags(function() {
-        this.model.get('tags').each(function(tag) {
-          var tagId = tag.get('_id');
-          tag.set('courses', this.model.get('courses').filter(function(course) {
-            return _.some(course.get('tags'), function(tag) { return tag._id === tagId });
-          }));
-        }, this);
-        console.log(this.model.get('tags'));
-        this.render();
+        this.fetchCourses(function() {
+          this.render();
+        });
       });
     },
 
     fetchTags: function(cb) {
       this.model.get('tags').fetch({
         data: { title: { $regex: '^' + TAG_PREFIX } },
-        success: _.bind(function() {
-          this.fetchCourses(cb);
+        success: _.bind(function(tags) {
+          this.processTags(tags);
+          cb.apply(this);
         }, this)
       });
+    },
+
+    processTags: function(tags) {
+      var m = [], p = [];
+      tags.each(function(tag) {
+        (tag.get('title')[0] === 'p') ? p.push(tag) : m.push(tag);
+      });
+      this.model.set({ moduleTags: m, projectTags: p });
     },
 
     fetchCourses: function(cb) {
@@ -51,8 +55,37 @@ define(function(require) {
             return { tags: tag.get('_id') };
           })
         },
-        success: _.bind(function() { cb.call(this); }, this)
+        success: _.bind(function(courses) {
+          this.processCourses(courses);
+          cb.apply(this);
+        }, this)
       });
+    },
+
+    processCourses: function(courses) {
+      courses.each(function(course) {
+        var mTag;
+        course.get('tags').forEach(function(tag) {
+          if(this.model.get('moduleTags').indexOf(tag._id)) {
+            mTag = tag;
+            return;
+          }
+        }, this);
+        course.set('moduleTag', mTag);
+      }, this);
+      this.model.get('projectTags').forEach(function(tag) {
+        tag.set('courses', courses.filter(function(course) {
+          return this.doesCourseHaveTag(course, tag);
+        }, this));
+      }, this);
+    },
+
+    doesCourseHaveTag: function(course, tag) {
+      var ret = false;
+      course.get('tags').forEach(function(courseTag) {
+        if(courseTag._id === tag.get('_id')) return ret = true;
+      });
+      return ret;
     }
   }, { template: 'tagMap' });
 
