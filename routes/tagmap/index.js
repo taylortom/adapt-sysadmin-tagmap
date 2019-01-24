65d14187f942c2cff6079d6c51c11fd626e66f51
Thomas Taylor <thomas.taylor@kineo.com>
2019-01-24 18:41:20 +0000

    Fix backend

--- routes/tagmap/index.js
+++ routes/tagmap/index.js
@@ -19,31 +19,48 @@ server.get('/api/tagmap', (req, res, next) => {
 
 function getTagMap() {
   return new Promise((resolve, reject) => {
-    getCourses().then(courses => {
+    Promise.all([getCourses(), getTags()]).then(data => {
+      const [courses,tags] = data;
       for(let i = 0, count = courses.length; i < count; i++) {
         const course = courses[i];
+
         if(!course.tags.length) {
           continue;
         }
         for(let j = 0, count2 = course.tags.length; j < count2; j++) {
           const tag = course.tags[j];
+
+          if(i === 0) console.log(tag.title);
+
           if(!course.projectTag && tag.title.match(PROJECT_PREFIX)) {
             course.projectTag = tag;
           } else if(!course.moduleTag && tag.title.match(MODULE_PREFIX)) {
             course.moduleTag = tag;
           }
         }
+        courses[i] = course;
       }
-      resolve(courses);
+      resolve({ courses, tags });
     }).catch(reject);
   });
 }
 
 function getCourses() {
   return new Promise((resolve, reject) => {
-    app.db.retrieve('course', {}, { populate: { tags: '_id title' } }, (error, courses) => {
+    return resolve(require('./courses.json'));
+    app.db.retrieve('course', { _isShared: true }, { populate: { tags: '_id title' }, jsonOnly: true }, (error, courses) => {
       if(error) return reject(error);
       resolve(courses);
+    });
+  });
+}
+
+function getTags() {
+  return new Promise((resolve, reject) => {
+    return resolve(require('./tags.json'));
+    app.db.retrieve('tag', { title: { $regex: PROJECT_PREFIX } }, { jsonOnly: true }, (error, tags) => {
+      if(error) return reject(error);
+      resolve(tags);
     });
   });
 }
